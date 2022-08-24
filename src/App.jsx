153fe710dabacbb4192bf2f07e8113e7790da150
Ronak Patel <ronak2715@gmail.com>
2022-08-23 22:09:57 -0400

    final

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,34 @@ import React from 'react';
 import './App.css';
 
 import UserInterface from "./UserInterface"
+import StylesContext from "./StylesContext";
 
 function App() {
+  const buttonStyles = {
+      barkground: "#1ECD97",
+      display: "inline-block",
+      width: 120,
+      height: 55,
+      fontSize: 18,
+      letterSpacing: 1,
+      border: "2px solid #1ECD97",
+      borderRadius: 15
+  };
+
   return (
     <main>
-     <h1
-          style={{
-            color: "#FEE001",
-            fontFamily: "Luminari, fantasy",
-            fontSize: 50
-          }}
-        >
-          Welcome to Comics Galore!
-        </h1>
-        <UserInterface />
+        <StylesContext.Provider value={buttonStyles}>
+            <h1
+                style={{
+                    color: "#FEE001",
+                    fontFamily: "Luminari, fantasy",
+                    fontSize: 50
+                }}
+                >
+                Welcome to Comics Galore!
+                </h1>
+                <UserInterface />
+        </StylesContext.Provider>
     </main>
   );
 }
